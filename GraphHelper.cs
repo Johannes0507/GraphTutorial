1549e560482af55724feb5d6e179fc579a29c5a4
keith <keithlee870507@gmail.com>
2024-03-01 17:24:07 +0800

    加入專案檔案。

---
+++ GraphHelper.cs
@@ -0,0 +1,13 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using MIcrosoft.Graph.Me.SendMail;
+
+class GraphHelper
+{
+    public static async Task<string> GetUserTokenAsync()
+    {
+
+    }
+}
